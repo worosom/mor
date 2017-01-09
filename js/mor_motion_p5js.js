779da705c6fee5e4ea82ec74ff69b91e5dafdd0c
Alexander Morosow <mor@boi.localdomain>
2017-01-09 03:40:48 +0100

    update

--- js/mor_motion_p5js.js
+++ js/mor_motion_p5js.js
@@ -10,24 +10,49 @@ var h = "innerHeight" in window
 var w = "innerWidth" in window 
                ? window.innerWidth
                : document.documentElement.offsetHeight;
-
-var num = 10;
+var c = 900000;
+var num = 100;
 var avgNum = 10;
 var maxNum = 2000;
 
 var population;
 
-var drag = .98;
-var speed = 1.;
-var groupForce = .02;
-var mgroupForce = .01;
-var repulsion = .1;
+var drag = .99;
+var speed = 1.*((w*h)/c);
+var groupForce = .2;
+var mgroupForce = .15;
+var repulsion = 1.;
 
 var targetFPS = 60;
 var avgFPS = 60;
 
+var flowFieldRes = 10;
+var flowField = [[], []];
+
+function FlowVec(pos) {
+  this.visc = .5;
+  this.pos = pos;
+  this.dir = p5.Vector.random2D();
+  this.rotate = function(to) {
+    var mag = to.mag();
+    to.normalize();
+    this.dir.add(p5.Vector.sub(to, this.dir).mult(-mag * this.visc));
+    this.dir.normalize();
+  }
+}
+
 function setup() {
   createCanvas(w, h, "WEBGL");
+  for(var _x = 0; _x <= flowFieldRes; _x++) {
+    var x = [];
+    for(var y = 0; y <= flowFieldRes *(h/w); y ++) {
+      var pos = createVector();
+      pos.x = _x / flowFieldRes * width;
+      pos.y = y / (flowFieldRes * (h/w)) * height;
+      x.push(new FlowVec(pos));
+    }
+    flowField.push(x);
+  }
   population = new Population();
   //generateParameters();
   //setInterval(population.update, 1./30.*1000.);
@@ -39,13 +64,18 @@ function draw() {
   stroke(0);
   strokeWeight(2.);
   population.display();
-  avgNum += (num - avgNum) * .01;
-  avgFPS += (frameRate() - avgFPS) * .1;
-  if (avgFPS < targetFPS - 4 && num > 0) {
-    num --;
-  } else if (avgFPS > targetFPS && num) {
-    num ++;
+  strokeWeight(1);
+  /*
+  for(var x = 0; x < flowField.length; x++) {
+    for(var y = 0; y < flowField[x].length; y++) {
+      push();
+      translate(flowField[x][y].pos.x, flowField[x][y].pos.y);
+      rotate(flowField[x][y].dir.heading());
+      line(2.5, 0, -2.5, 0);
+      pop();
+    }
   }
+   */
 }
 
 function Population() {
@@ -58,28 +88,41 @@ function Population() {
   this.update = function() {
    for(var i = 0; i < this.cells.length; i++) {
     this.cells[i].update();
-    var velMagSq = this.cells[i].getVelMagSq();
-    velMagSq *= 4.;
-    velMagSq += 1.;
-    this.cells[i].addForce(p5.Vector.random2D().mult(1/velMagSq));
+    var velMag = this.cells[i].vel.mag();
+     velMag *= 100.;
+     velMag += .1;
+    //this.cells[i].addForce(p5.Vector.random2D().mult(1/velMag));
 
     for (var j = 0; j < this.cells.length; j++) {
       if (i != j) {
         var dir = p5.Vector.sub(this.cells[i].pos, this.cells[j].pos);
-        //dir.normalize();
         var dist = dir.x*dir.x+dir.y*dir.y;
         var force = cubicPulse(400, 100, dist);
         var mForce = cubicPulse(1000, 4900, dist);
         var repForce = cubicPulse(0, 400, dist);
-        this.cells[i].addForce(dir.copy().mult(force * groupForce * this.cells[i].getDna().x));
-        this.cells[j].addForce(dir.copy().mult(-force * groupForce) * this.cells[j].getDna().y);
 
-        this.cells[i].addForce(
-            p5.Vector.sub(this.cells[i].vel, this.cells[j].vel).mult(
-              mForce * mgroupForce * this.cells[i].getDna().z));
+        var forceVec = dir.copy();
+        forceVec.mult(force * groupForce * this.cells[i].getDna().x);
+        this.cells[i].addForce(forceVec);
+        this.cells[j].addForce(forceVec.mult(-1.));
 
-        this.cells[i].addForce(dir.copy().mult(
-              - repForce * 1/dist * repulsion));
+        forceVec = p5.Vector.sub(this.cells[i].vel, this.cells[j].vel);
+        forceVec.mult(mForce * mgroupForce * this.cells[i].getDna().z);
+        this.cells[i].addForce(forceVec);
+
+        forceVec = dir.copy().mult(- repForce * 1/dist * repulsion);
+        this.cells[i].addForce(forceVec);
+      }
+    }
+     
+    for(var x = 0; x < flowField.length; x++) {
+      for(var y = 0; y < flowField[x].length; y++) {
+        var dir = p5.Vector.sub(this.cells[i].pos, flowField[x][y].pos);
+        var distSq = dir.x * dir.x + dir.y * dir.y;
+        var force = cubicPulse(0, 2500*2, distSq);
+        this.cells[i].rotate(dir.normalize(), force);
+        this.cells[i].addForce(dir.normalize().mult(force*.01));
+        flowField[x][y].rotate(this.cells[i].vel.copy().mult(force));
       }
     }
     this.cells[i].update();
@@ -91,13 +134,23 @@ function Population() {
   }
 
   this.display = function() {
-   if(num > this.cells.length && random(1) > .99)
-    this.cells.push(new Cell());
-   else if(num < this.cells.length && random(1))
-    this.cells.pop();
-
+    var minDist = width/flowFieldRes;
+    var minDistSq = minDist * minDist;
+    strokeWeight(1);
     for(var i = 0; i < this.cells.length; i++) {
-      this.cells[i].display();
+      var connections = 0;
+      for(var j = 0; j < this.cells.length; j++) {
+        if(i != j) {
+          var dir = p5.Vector.sub(this.cells[j].pos, this.cells[i].pos);
+          var distSq = dir.x * dir.x + dir.y * dir.y;
+          if(distSq < minDistSq && connections < 5) {
+            stroke(0, map(distSq, 0, minDistSq, 127, 0));
+            line(this.cells[j].pos.x, this.cells[j].pos.y, this.cells[i].pos.x, this.cells[i].pos.y);
+            connections ++;
+          }
+        }
+      }
+      //this.cells[i].display();
     }
   }
 }
@@ -133,8 +186,15 @@ function Cell(pos, vel, dna) {
     this.vel.add(force);
   }
 
+  this.rotate = function(to, force) {
+    var mag = to.mag();
+    to.normalize();
+    this.vel.add(p5.Vector.sub(to, this.dir).mult(mag * force));
+  }
+
   this.update = function() {
     this.vel.mult(drag);
+    //this.vel.add(this.getDna().x*.5, this.getDna().y*.5);
     this.pos.add(this.vel.copy().mult(speed));
     this.time += 1;
     this.time %= this.dna.seq.length;
@@ -160,7 +220,7 @@ function DNA(seq, length) {
   } else {
     this.seq = [];
     for(var i = 0; i < length; i++) {
-      this.seq.push(p5.Vector.random2D().mult(random(10)));
+      this.seq.push(p5.Vector.random2D().add(1, 1).mult(random(1)));
     }
   }
 
@@ -175,4 +235,13 @@ function DNA(seq, length) {
     }
     return newSeq;
   }
+
+  this.clone = function() {
+    var temp = new DNA(this.seq, null);
+    return temp;
+  }
+}
+
+function Brain(dna) {
+  this.dna = dna;
 }
